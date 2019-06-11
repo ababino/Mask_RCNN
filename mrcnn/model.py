fdb62d95d2e006d14ab3a919854ca9ffdc5b343c
Andres <ababino@gmail.com>
2019-06-11 14:03:30 -0400

    add keypoint header constructor function

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1005,6 +1005,90 @@ def build_fpn_mask_graph(rois, feature_maps, image_meta,
     return x
 
 
+def build_fpn_keypoint_mask_graph(rois, feature_maps, image_meta,
+                                  pool_size, num_keypoints, train_bn=True):
+    """Builds the computation graph of the mask head of Feature Pyramid Network.
+
+    rois: [batch, num_rois, (y1, x1, y2, x2)] Proposal boxes in normalized
+          coordinates.
+    feature_maps: List of feature maps from different layers of the pyramid,
+                  [P2, P3, P4, P5]. Each has a different resolution.
+    image_meta: [batch, (meta data)] Image details. See compose_image_meta()
+    pool_size: The width of the square feature map generated from ROI Pooling.
+    num_keypoints: number of classes, which determines the depth of the results
+    train_bn: Boolean. Train or freeze Batch Norm layers
+
+    Returns: Masks [batch, num_rois, MASK_POOL_SIZE, MASK_POOL_SIZE, NUM_CLASSES]
+    """
+    # ROI Pooling
+    # Shape: [batch, num_rois, MASK_POOL_SIZE, MASK_POOL_SIZE, channels]
+    x = PyramidROIAlign([pool_size, pool_size],
+                        name="roi_align_mask")([rois, image_meta] + feature_maps)
+
+    # Conv layers
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv1")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn1')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv2")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn2')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv3")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn3')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv4")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn4')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv5")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn5')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv6")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn6')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv7")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn7')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    x = KL.TimeDistributed(KL.Conv2D(512, (3, 3), padding="same"),
+                           name="mrcnn_keypoint_conv8")(x)
+    x = KL.TimeDistributed(BatchNorm(),
+                           name='mrcnn_keypoint_mask_bn8')(x, training=train_bn)
+    x = KL.Activation('relu')(x)
+
+    # Deconv Layer
+    x = KL.TimeDistributed(KL.Conv2DTranspose(256, (4, 4), strides=2,
+                                              activation="relu"),
+                           name="mrcnn_keypoint_mask_deconv")(x)
+
+    # Upscaling
+    x = KL.TimeDistributed(KL.Lambda(lambda y: tf.image.resize_bilinear(y, [56, 56])),
+                           name="mrcnn_keypoint_mask_upscaling2x")(x)
+
+    # Final convolution without activation. Softmax is used in the loss function
+    x = KL.TimeDistributed(KL.Conv2D(num_keypoints, (1, 1), strides=1),
+                           name="mrcnn_keypoint_mask")(x)
+    return x
+
+
 ############################################################
 #  Loss Functions
 ############################################################
